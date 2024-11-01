6815f0d65419d7cf42b407421a6e65fa86a33796
J <mosinabrejp@students.nu-dasma.edu.ph>
2024-11-01 19:01:22 +0800

    change date order

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# app.py
-
 from flask import Flask, render_template, jsonify
 import pandas as pd
 import numpy as np
@@ -9,12 +7,13 @@ from sklearn.metrics import mean_squared_error
 
 app = Flask(__name__)
 
-# Load and preprocess data
+
 df = pd.read_csv('PSE.csv')
 df['Date'] = pd.to_datetime(df['Date'])
+df.sort_values('Date', inplace=True)
 df.set_index('Date', inplace=True)
 
-# Convert shorthand notation to numerical values in 'Vol.'
+
 def convert_shorthand_to_num(value):
     if 'K' in value:
         return float(value.replace('K', '')) * 1e3
@@ -24,12 +23,11 @@ def convert_shorthand_to_num(value):
         return float(value.replace('B', '')) * 1e9
     else:
         return float(value)
+    
 df['Vol.'] = df['Vol.'].apply(convert_shorthand_to_num)
-
-# Convert 'Change' column from percentages to floats
 df['Change'] = df['Change'].str.replace('%', '').astype(float) / 100
 
-# Prepare data
+
 X = df[['Open', 'High', 'Low', 'Vol.', 'Change']].values
 y = df['Price'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False)
